bd2fab8eabb45507bf98f7fee28d8d2e7abe2250
Saad Iqbal <s1iqbal@ryerson.ca>
2019-04-24 23:38:20 -0400

    h950p driver added

---
+++ Huion H950P Driver/amd64/tabetdriveruninstall.bat
@@ -0,0 +1,3 @@
+DIFxCmd /u TabletDriver.inf DRIVER_PACKAGE_FORCE DRIVER_PACKAGE_DELETE_FILES
+DIFxCmd.exe /u vmulti.inf DRIVER_PACKAGE_FORCE DRIVER_PACKAGE_DELETE_FILES
+devcon.exe remove huion\tablethid 
